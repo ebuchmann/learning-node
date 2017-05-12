2c4b9a91343deb9d23b95cea2c7dd82c8a26bdd9
Eric Buchmann <me@ebuchmann.com>
2017-05-11 19:46:35 -0700

    start of controllers

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  res.render('hello', {
-    name: 'Eric',
-  });
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;

e00f571aa5ee5cb6186202a87abd86fe27c488df
Eric Buchmann <me@ebuchmann.com>
2017-05-11 20:22:59 -0700

    start of models

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', storeController.homePage);
+router.get(
+  '/',
+  storeController.homePage
+);
 
 module.exports = router;

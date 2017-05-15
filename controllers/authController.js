919aed35b68ceed2bf6fd9e386225a3a69220641
Eric Buchmann <me@ebuchmann.com>
2017-05-14 17:52:33 -0700

    authentication

---
+++ controllers/authController.js
@@ -0,0 +1,21 @@
+const passport = require('passport');
+
+exports.login = passport.authenticate('local', {
+  failureRedirect: '/login',
+  failureFlash: 'Failed login',
+  successRedirect: '/',
+  successFlash: 'You are now logged in',
+})
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'You are now logged out');
+  res.redirect('/');
+}
+
+exports.isLoggedIn = (req, res, next) => {
+  if(req.isAuthenticated()) return next();
+
+  req.flash('error', 'You must be logged in');
+  res.redirect('/login');
+}

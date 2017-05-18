b9751685c80d0469513020945801486544368765
Eric Buchmann <me@ebuchmann.com>
2017-05-17 18:56:28 -0700

    store reviews

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -49,10 +50,14 @@ router.post('/account/reset/:token',
 
 router.get('/map', storeController.mapPage);
 
+router.get('/hearts', catchErrors(storeController.getHearts));
+
+router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));
+
 // Api routes
 
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
-router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
+router.post('/api/stores/:id/heart', authController.isLoggedIn, catchErrors(storeController.heartStore));
 
 module.exports = router;
